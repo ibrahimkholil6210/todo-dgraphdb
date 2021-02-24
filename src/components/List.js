3d121478dc52e4b758001f3f11dbeed1ab725aa0
Ibrahim <ibrahimkholil550@gmail.com>
2021-02-24 19:53:14 +0600

    refactored

--- src/components/List.js
+++ src/components/List.js
@@ -10,8 +10,8 @@ const StyledAvatar = styled(Avatar)`
 `;
 
 const ListRen = ({ data }) => {
-  const [_, updateTodo] = useMutation(UpdateTodo);
-  const [__, deleteTodo] = useMutation(DeleteTodo);
+  const [updateTodoresult, updateTodo] = useMutation(UpdateTodo);
+  const [deleteTodoResult, deleteTodo] = useMutation(DeleteTodo);
 
   const handleUpdate = (id, completed) => {
     const variables = {
